75029bbf937c2f946d87f5a3503dedb2870370c5
Documenter.jl <documenter@juliadocs.github.io>
2022-05-30 19:44:06 +0000

    build based on 387a8a7

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Stingray","category":"page"},{"location":"#Stingray","page":"Home","title":"Stingray","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Stingray.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Stingray]","category":"page"}]
+}
