0bceba00892efe8f7d954ea18feead6b2285b34d
kashish shrivastav <en.shrivastavkashish@gmail.com>
2025-03-07 16:40:03 +0530

    add trace states and limit angle 360 and hue line data

--- src/power_colors.jl
+++ src/power_colors.jl
@@ -2,6 +2,7 @@ using Plots
 using Interpolations
 using LinearAlgebra
 using colors
+using Interpolations
 
 DEFAULT_COLOR_CONFIGURATION = Dict(
     "center" => [4.51920, 0.453724],
@@ -87,3 +88,52 @@ function _create_rms_hue_plot(; polar=false, plot_spans=false, configuration=DEF
 
     return fig
 end
+
+function _limit_angle_to_360(angle)
+    while angle >= 360
+        angle -= 360
+    end
+    while angle < 0
+        angle += 360
+    end
+    return angle
+end
+
+function _hue_line_data(center, angle, ref_angle=3 * pi / 4)
+    plot_angle = mod(-angle + ref_angle, 2 * pi)
+
+    m = tan(plot_angle)
+    if isinf(m)
+        x = fill(center[1], 20)
+        y = range(-4, stop=4, length=20)
+    else
+        x = range(0, stop=4, length=20) .* sign(cos(plot_angle)) .+ center[1]
+        y = center[2] .+ m .* (x .- center[1])
+    end
+
+    function _trace_states(ax, configuration=DEFAULT_COLOR_CONFIGURATION; kwargs...)
+        center = log10.(configuration["center"])
+        for (state, details) in configuration["state_definitions"]
+            color = details["color"]
+            hue0, hue1 = details["hue_limits"]
+            hue_mean = (hue0 + hue1) / 2
+            hue_angle = mod(-deg2rad(hue_mean) + 3 * pi / 4, 2 * pi)
+    
+            radius = 1.4
+            txt_x = radius * cos(hue_angle) + center[1]
+            txt_y = radius * sin(hue_angle) + center[2]
+            annotate!(ax, txt_x, txt_y, text(state, :black, :center))
+    
+            x0, y0 = _hue_line_data(center, deg2rad(hue0), ref_angle=configuration["ref_angle"])
+            next_angle = hue0 + 5.0
+            x1, y1 = _hue_line_data(center, deg2rad(hue0), ref_angle=configuration["ref_angle"])
+    
+            while next_angle <= hue1
+                x0, y0 = x1, y1
+                x1, y1 = _hue_line_data(center, deg2rad(next_angle), ref_angle=configuration["ref_angle"])
+                
+                polygon!(ax, [x0[1], x0[end], x1[end]], [y0[1], y0[end], y1[end]], color=color, lw=0, label="")
+                next_angle += 5.0
+            end
+        end
+    end
