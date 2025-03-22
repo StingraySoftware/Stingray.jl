29d5c33435f7ba72cae46da1f5660bb6fe6719ae
kashish shrivastav <en.shrivastavkashish@gmail.com>
2025-03-22 10:31:22 +0530

    add readevent

---
+++ src/events.jl
@@ -0,0 +1,43 @@
+struct Meta
+    headers::Vector{Dict{String,Any}}
+end
+
+struct EventList{T}
+    filename::String
+    times::Vector{T}
+    energies::Vector{T}
+    metadata::Meta
+end
+
+function readevents(path; T=Float64)
+    headers = Dict{String,Any}[]
+    times = T[]
+    energies = T[]
+    
+    FITS(path, "r") do f
+        for i in 1:length(f)  # Iterate over HDUs
+            hdu = f[i]
+            header_dict = Dict{String,Any}()
+            for key in keys(read_header(hdu))
+                header_dict[string(key)] = read_header(hdu)[key]
+            end
+            push!(headers, header_dict)
+            
+            # Check if the HDU is a table
+            if isa(hdu, TableHDU)
+                # Get column names using the correct FITSIO method
+                colnames = FITSIO.colnames(hdu)
+                
+                if "TIME" in colnames
+                    times = convert(Vector{T}, read(hdu, "TIME"))
+                end
+                if "ENERGY" in colnames
+                    energies = convert(Vector{T}, read(hdu, "ENERGY"))
+                end
+            end
+        end
+    end
+    
+    metadata = Meta(headers)
+    return EventList{T}(path, times, energies, metadata)
+end
