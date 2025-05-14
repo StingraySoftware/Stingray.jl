2b231108a072de8de765ee43c77f73f458d87d44
kashish shrivastav <en.shrivastavkashish@gmail.com>
2025-05-14 18:20:33 +0530

    add configure_fftw_for_macos in runtest.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,6 +2,28 @@ using Stingray
 using Test
 using FFTW, Distributions, Statistics, StatsBase, HDF5, FITSIO
 using Logging
+using Pkg
+using InteractiveUtils
+# this function is for temporary support we can remove after the update of FFTW.jl
+# to use the system FFTW library on macOS
+function configure_fftw_for_macos()
+    @info "Configuring FFTW for macOS..."
+    fftw_env_vars = ["FFTW_LIBRARY_PATH", "FFTW_LIBRARY", "FFTW_FORCE_SYSTEM_LIB"]
+    for var in fftw_env_vars
+        if haskey(ENV, var)
+            @info "Removing existing FFTW environment variable: $var"
+            delete!(ENV, var)
+        end
+    end
+    @info "Setting up FFTW with FFTW_jll provider..."
+    FFTW.set_provider!("fftw")
+    Pkg.build("FFTW")
+end
+
+if Sys.isapple()
+    @info "macOS detected, applying FFTW configurations..."
+    configure_fftw_for_macos()
+end
 include("test_fourier.jl")
 include("test_gti.jl")
 include("test_events.jl")
