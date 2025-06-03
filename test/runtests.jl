8f643074d7c2d7c167fe4ff863a4d06756a31dcd
fjebaker <fergus@cosroe.com>
2025-06-03 15:03:51 +0100

    test: fetch testdata from remote server

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,4 +4,29 @@ using FFTW, Distributions, Statistics, StatsBase, HDF5, FITSIO
 using Logging
 include("test_fourier.jl")
 include("test_gti.jl")
-include("test_events.jl")
+
+TEST_DATA_PATH = joinpath(@__DIR__, "_data")
+
+TEST_DATA_REMOTE = "https://www.star.bristol.ac.uk/fergus/stingray/testdata.tar.gz"
+TEST_DATA_SHA256 = "cd5311e5ea1aaf7eef8253a369bd8ba6fbc10c922a22281adc14be61dc6e468c"
+
+# assert the test data is available
+if !isdir(TEST_DATA_PATH)
+    using Downloads, Tar, CodecZlib, SHA
+    dest = TEST_DATA_PATH * ".tar.gz"
+    Downloads.download(TEST_DATA_REMOTE, dest)
+
+    open(dest) do tar_gz
+        @assert bytes2hex(sha256(tar_gz)) == TEST_DATA_SHA256
+        tar = GzipDecompressorStream(tar_gz)
+        Tar.extract(tar, TEST_DATA_PATH)
+    end
+
+    rm(dest)
+else
+    @info "Test data is present"
+end
+
+@testset "Eventlist" begin
+    include("test_events.jl")
+end
