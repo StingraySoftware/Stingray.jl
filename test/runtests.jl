232735639e52ce5ec2d498963d3a8ee23c712a81
kashish shrivastav <en.shrivastavkashish@gmail.com>
2025-09-21 16:57:11 +0530

    updating tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,31 +6,31 @@ using CFITSIO
 using Random
 using Plots
 
-@testset "Fourier" begin
+@testset verbose = true "Fourier" begin
     include("test_fourier/test_fourier.jl")
     include("test_fourier/test_coherence.jl")
     include("test_fourier/test_norm.jl")
 end
-@testset "GTI" begin
+@testset verbose = true "GTI" begin
     include("test_gti.jl")
 end
-@testset "Eventlist" begin
+@testset verbose = true "Eventlist" begin
     include("test_events.jl")
 end
-@testset verbose = true "Synthetic Events Tests" begin
+@testset verbose = true "mission_support" begin
     include("test_missionSupport.jl")
 end
-@testset "lightcurve" begin
+@testset verbose = true "lightcurve" begin
     include("test_lightcurve.jl")
 end
-@testset "powerspectrum" begin
+@testset verbose = true "powerspectrum" begin
     include("test_powerspectrum.jl")
 end
-@testset "crossspectrum" begin
+@testset verbose = true "crossspectrum" begin
     include("test_crossspectrum.jl")
 end
 
-@testset "recipes" begin
+@testset verbose = true "recipes" begin
     include("test_plotting/test_plots_recipes_lightcurve.jl")
     include("test_plotting/test_plots_recipes_gti.jl")
     include("test_plotting/test_plots_recipes_powerspectrum.jl")
